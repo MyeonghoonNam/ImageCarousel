c3726111723698b355cc18f56fec5d2832b5e493
MyeongHoonNam <codenmh0822@naver.com>
2022-05-09 23:20:01 +0900

    feat: 이미지 업로드를 통한 캐러셀 업데이트 기능 함수 구현

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,41 @@
 (() => {
   const carouselUl = document.querySelector(".carousel-list");
+  const imageInput = document.querySelector("#image-upload-input");
   const nextButton = document.querySelector(".next-btn");
   const prevButton = document.querySelector(".prev-btn");
+
+  const createImageEl = (url) => {
+    const list = document.createElement("li");
+    const img = document.createElement("img");
+
+    list.classList.add("carousel-item");
+    img.src = url;
+    list.appendChild(img);
+
+    const images = document.querySelectorAll(".carousel-item");
+    images.forEach((image) => {
+      image.classList.remove("now");
+    });
+
+    list.classList.add("now");
+    return list;
+  };
+
+  const uploadImage = (e) => {
+    const images = document.querySelectorAll(".carousel-item");
+    const value = e.target;
+
+    if (value.files) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const imageUrl = e.target.result;
+        carouselUl.insertBefore(createImageEl(imageUrl), images[0]);
+      };
+
+      reader.readAsDataURL(value.files[0]);
+    }
+  };
 
   const moveNext = () => {
     const images = document.querySelectorAll(".carousel-item");
@@ -29,6 +63,7 @@
     }
   };
 
+  imageInput.addEventListener("change", uploadImage);
   nextButton.addEventListener("click", moveNext);
   prevButton.addEventListener("click", movePrev);
 })();
