dd2d25e0cdb396dfe88847b975116ea562d42a5a
MyeongHoonNam <codenmh0822@naver.com>
2022-05-09 22:38:53 +0900

    feat: 이전 이미지를 보여주는 movePrev 함수 구현

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 (() => {
   const carouselUl = document.querySelector(".carousel-list");
   const nextButton = document.querySelector(".next-btn");
+  const prevButton = document.querySelector(".prev-btn");
 
   const moveNext = () => {
     const images = document.querySelectorAll(".carousel-item");
@@ -15,5 +16,19 @@
     }
   };
 
+  const movePrev = () => {
+    const images = document.querySelectorAll(".carousel-item");
+
+    if (images.length > 1) {
+      const current_image = document.querySelector(".now");
+      const last_image = carouselUl.lastElementChild;
+
+      carouselUl.insertBefore(last_image, images[0]);
+      current_image.classList.remove("now");
+      last_image.classList.add("now");
+    }
+  };
+
   nextButton.addEventListener("click", moveNext);
+  prevButton.addEventListener("click", movePrev);
 })();
